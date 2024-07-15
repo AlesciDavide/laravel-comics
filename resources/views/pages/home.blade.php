e867804fa5d7260e5da3b26ade4e326b73868ba1
AlesciDavide <davi.alesci@Gmail.com>
2024-07-15 17:19:11 +0200

    importazione header e main

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,8 +1,57 @@
 @extends('layout.app')
 @section('page-title', 'Homepage')
 
-    @section('main-content')
 
-    <h1>Ciao, qua dentro inserisci il contenuto del main con yield</h1>
 
-    @endsection
+@section('jumbo-tron')
+        <section class="container-jumbo">
+            <div><p>current series</p></div>
+        </section>
+@endsection
+
+
+
+
+@section('main-content')
+    <section class="list-products">
+        @foreach ($comics as $comic)
+    <article class="series_comics">
+            <img src="{{ $comic['thumb'] }}" alt="{{$comic['title']}}">
+            <p>
+                {{$comic['title']}}
+            </p>
+        </article>
+        @endforeach
+        <div><p>Load more</p></div>
+    </section>
+@endsection
+
+@section('info')
+
+<section class="container-main">
+    <ul>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-digital-comics.png')}}" alt="">
+            digital comics
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-merchandise.png')}}" alt="">
+            dc merchandise
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-subscriptions.png')}}" alt="">
+            subscription
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-comics-shop-locator.png')}}" alt="">
+            comic shop locator
+        </li>
+        <li>
+            <img src="{{ Vite::asset('resources/img/buy-dc-power-visa.svg')}}" alt="">
+            dc power visa
+        </li>
+    </ul>
+
+</section>
+
+@endsection
